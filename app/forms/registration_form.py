865d14d3989d63c302be5c2d4f181825e4a22ec7
yahaiii <wanekaxa@gmail.com>
2023-10-18 15:51:58 +0100

    add soft reg && login

--- app/forms/registration_form.py
+++ app/forms/registration_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 
 class RegistrationForm(FlaskForm):
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -11,7 +11,7 @@ def register():
     """
     Register a new user.
 
-    This function handles the registration process for a new user. It receives a POST request with the user's registration details and creates a new user account if the provided information is valid. If the registration is successful, the user is redirected to the login page. If there are validation errors or if the username or email is already taken, appropriate error messages are flashed and the registration form is rendered again.
+    This function handles the registration process for a new user. It receives a POST request with the user's registration details and creates a new user account if the provided information is valid. If the registration is successful, the user is redirected to the login page. If there are validation errors or if the username or email is already taken, appropriate error messages are flashed, and the registration form is rendered again.
 
     Parameters:
     - None
@@ -24,32 +24,36 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        # Form submission
-        username = form.username.data
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        email = form.email.data
-        password = form.password.data
+        try:
+            # Form submission
+            username = form.username.data
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            email = form.email.data
+            password = form.password.data
 
-        # Check if the username and email are unique
-        existing_user = User.query.filter_by(username=username).first()
-        existing_email = User.query.filter_by(email=email).first()
+            # Check if the username and email are unique
+            existing_user = User.query.filter_by(username=username).first()
+            existing_email = User.query.filter_by(email=email).first()
 
-        if existing_user:
-            flash('This username is already taken. Please choose a different one.', 'danger')
-        elif existing_email:
-            flash('An account with this email already exists. Please use a different email address.', 'danger')
-        else:
-            # Create a new user and set the password
-            new_user = User(username=username, first_name=first_name, last_name=last_name, email=email)
-            new_user.set_password(password)
+            if existing_user:
+                flash('This username is already taken. Please choose a different one.', 'danger')
+            elif existing_email:
+                flash('An account with this email already exists. Please use a different email address.', 'danger')
+            else:
+                # Create a new user and set the password
+                new_user = User(username=username, first_name=first_name, last_name=last_name, email=email)
+                new_user.set_password(password)
 
-            # Add the new user to the database
-            db.session.add(new_user)
-            db.session.commit()
+                # Add the new user to the database
+                db.session.add(new_user)
+                db.session.commit()
 
-            flash('Your account has been created! You can now log in.', 'success')
-            return redirect(url_for('auth.login'))
+                flash('Your account has been created! You can now log in.', 'success')
+                return redirect(url_for('auth.login'))
+
+        except Exception as e:
+            flash('An error occurred during registration. Please try again later.', 'danger')
 
     return render_template('registration.html', form=form)
 
